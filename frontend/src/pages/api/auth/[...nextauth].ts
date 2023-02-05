1df50112f5e057874a0fe1a544ba90039e1fa9dc
klam2k20 <klam2k17@gmail.com>
2023-02-05 00:04:20 -0500

    Init commit - get nextAuth functional

---
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+
+/**
+ * All requests to /api/auth/* (signIn, callback, signOut, etc.)
+ * will automatically be handled by NextAuth.js
+ */
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+})
