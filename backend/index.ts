26e0cdaaa96887d758fdabf52d69bb1597a308b0
klam2k20 <klam2k17@gmail.com>
2023-02-05 21:56:49 -0500

    Clarifying comment aboyt graphql schemas

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,17 @@ import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDe
 import express from 'express';
 import http from 'http';
 
+/**
+ * A GraphQL schema defines the entity types - a description of the data
+ * clients can request from a GraphQL API and operation types - the 
+ * queries, mutations, and subscription functions that the client
+ * can use to read and write data from the GraphQL server. The main 
+ * purpose of having a schema is to make sure our GraphQL API is type safe.
+ * Any requests to and any entities being sent to or returned from our API
+ * most match a defined entity or operation type
+ * @param typeDefs defines the entity types
+ * @param resolvers defines the operation types
+ */
 async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
