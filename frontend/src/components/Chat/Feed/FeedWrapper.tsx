3a0c27ea00827233e63d6c081a79803814be1b72
klam2k20 <klam2k17@gmail.com>
2023-02-09 15:48:05 -0500

    Add message feed header and add back button

--- frontend/src/components/Chat/Feed/FeedWrapper.tsx
+++ frontend/src/components/Chat/Feed/FeedWrapper.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text } from '@chakra-ui/react';
 import { Session } from 'next-auth';
 import { useRouter } from 'next/router';
+import MessageHeader from './Messages/MessageHeader';
 
 interface IFeedWrapperProps {
   session: Session;
@@ -9,12 +10,21 @@ interface IFeedWrapperProps {
 const FeedWrapper: React.FC<IFeedWrapperProps> = ({ session }) => {
   const router = useRouter();
   const { conversationId } = router.query;
+  const {
+    user: { id: userId },
+  } = session;
   return (
     <Flex
       display={{ base: conversationId ? 'flex' : 'none', md: 'flex' }}
       w="100%"
       flexDirection="column">
-      {conversationId ? <Flex>{conversationId}</Flex> : <Text>No Conversation Selected</Text>}
+      {conversationId ? (
+        <Flex flexDirection="column" justify="space-between" flexGrow={1}>
+          <MessageHeader userId={userId} conversationId={conversationId as string} />
+        </Flex>
+      ) : (
+        <Text>No Conversation Selected</Text>
+      )}
     </Flex>
   );
 };
---
+++ frontend/src/components/Chat/Feed/Messages/MessageHeader.tsx
@@ -0,0 +1,60 @@
+import { ConversationPopulated } from '@/../backend/src/util/types';
+import { formatAvatars, formatConversationName } from '@/src/util/functions';
+import { ConversationsResponse } from '@/src/util/types';
+import { useQuery } from '@apollo/client';
+import { Avatar, AvatarGroup, Button, Flex, Text } from '@chakra-ui/react';
+import conversationOperations from '../../../../graphql/operations/conversation';
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { useRouter } from 'next/router';
+
+interface IMessageHeaderProps {
+  userId: string;
+  conversationId: string;
+}
+
+const MessageHeader: React.FC<IMessageHeaderProps> = ({ userId, conversationId }) => {
+  const { data, loading, error } = useQuery<ConversationsResponse>(
+    conversationOperations.Queries.conversations
+  );
+  const conversation = data?.conversations.find(
+    c => c.id === conversationId
+  ) as ConversationPopulated;
+  const formatAvatar = formatAvatars(userId, conversation.participants);
+  const router = useRouter();
+
+  return (
+    <Flex
+      p={4}
+      align="center"
+      borderBottom="1px solid"
+      borderColor="whiteAlpha.200"
+      justify="space-between">
+      <Button
+        display={{ md: 'none' }}
+        bg="#2d2d2d"
+        _hover={{ bg: '#2d2d2d' }}
+        onClick={() => router.replace('?conversationId', '/', { shallow: true })}>
+        <MdArrowBackIosNew fontSize={16} color="#3d84f7" />
+      </Button>
+
+      <Flex flexDirection="column" h="100%" w="100%" align="center">
+        {conversation.participants.length == 2 ? (
+          <Avatar src="" name={formatAvatar[0]} />
+        ) : (
+          <AvatarGroup size="md" max={2}>
+            {formatAvatar.map(username => (
+              <Avatar key={username} src="" name={username} />
+            ))}
+          </AvatarGroup>
+        )}
+        <Text fontWeight={600}>
+          {conversation.participants.length == 2
+            ? formatConversationName(userId, conversation.participants)
+            : `${conversation.participants.length - 1} People`}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default MessageHeader;
