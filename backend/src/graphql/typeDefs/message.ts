bc45196e331014ffbabb9b0bbb611147cee107ea
klam2k20 <klam2k17@gmail.com>
2023-02-10 13:13:22 -0500

    Update message typedef to include query, mutation and subscription

--- backend/src/graphql/typeDefs/message.ts
+++ backend/src/graphql/typeDefs/message.ts
@@ -6,11 +6,26 @@ scalar Date
   type Message {
     id: String
     sender: User
+    conversation: Conversation
     body: String
     # isLatestIn: Conversation
     createdAt: Date
     updatedAt: Date
   }
+
+  # Query all messages for the specified conversation
+  type Query {
+    messages(conversationId: string): [Message]
+  }, 
+
+  type Mutation {
+    sendMessage(id: String, senderId: String, conversationId: String, body: String)
+  },
+
+  # Subscribe to the following conversation to get updated messages
+  type Subscription {
+    messageSent(conversationId: String)
+  },
 `
 
 export default typeDefs;
