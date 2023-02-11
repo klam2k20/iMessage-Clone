2d3e07757fe5cc938ef65ddcd538e290beca4ae5
klam2k20 <klam2k17@gmail.com>
2023-02-11 14:51:48 -0500

    Add message gql queries

---
+++ frontend/src/graphql/operations/message.ts
@@ -0,0 +1,41 @@
+import { gql } from "@apollo/client";
+
+const messagePopulated = `
+  id
+  sender {
+    id
+    username  
+  }
+  body
+  createdAt
+`;
+
+const messageOperations = {
+  Queries: {
+    messages: gql`
+      query messages($conversationId: String!) {
+        messages(conversationId: $conversationId) {
+          ${messagePopulated}
+        }
+      }
+    `,
+  },
+  Mutations: {
+    sendMessage: gql`
+      mutation sendMessage($id: String!, $senderId: String!, $conversationId: String!, $body: String!) {
+        sendMessage(id: $id, senderId: $senderId, conversationId: $conversationId, body: $body)
+      }
+    `,
+  },
+  Subscriptions: {
+    messageSent: gql`
+      subscription messageSent($conversationId: String!) {
+        messageSent(conversationId: $conversationId) {
+          ${messagePopulated}
+        }
+      }
+    `,
+  }
+}
+
+export default messageOperations;
