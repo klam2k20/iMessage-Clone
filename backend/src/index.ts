93009d2479406b8f6d4184628e4b1f51c3c9dced
klam2k20 <klam2k17@gmail.com>
2023-02-15 18:31:42 -0500

    Move port to env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -95,7 +95,7 @@ async function startApolloServer() {
       },
     }),
   );
-  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise<void>((resolve) => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
   console.log('🚀 Server ready at http://localhost:4000/graphql');
 }
 
