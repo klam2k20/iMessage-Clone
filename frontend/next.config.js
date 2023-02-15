648e306f22e0bbea6ef105ddfb8d5e494b1c141d
klam2k20 <klam2k17@gmail.com>
2023-02-15 15:05:06 -0500

    Configure eslint to only run in frontend src folder

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['pages', '/frontend/src'], 
+  },
+};
 
 module.exports = nextConfig
