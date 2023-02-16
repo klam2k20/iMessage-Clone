2e56af22abe54f2c7905d70780896289c8cb0333
klam2k20 <klam2k17@gmail.com>
2023-02-16 02:37:44 -0500

    rename delete-conversation gif

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,23 +1,27 @@
-import { ApolloProvider } from "@apollo/client";
-import { ChakraBaseProvider } from "@chakra-ui/react";
-import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
-import { Toaster } from "react-hot-toast";
-import { theme } from "../chakra/theme";
-import { client } from "../graphql/apollo-client";
+import { ApolloProvider } from '@apollo/client';
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Toaster } from 'react-hot-toast';
+import { theme } from '../chakra/theme';
+import { client } from '../graphql/apollo-client';
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <SessionProvider session={session}>
-        <ChakraBaseProvider theme={theme}>
-          <Component {...pageProps} />
-          <Toaster position='bottom-center' />
-        </ChakraBaseProvider>
-      </SessionProvider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <meta name="description" content="A Real-time Chat App created with React" />
+        <title>iMessage</title>
+      </Head>
+      <ApolloProvider client={client}>
+        <SessionProvider session={session}>
+          <ChakraBaseProvider theme={theme}>
+            <Component {...pageProps} />
+            <Toaster position="bottom-center" />
+          </ChakraBaseProvider>
+        </SessionProvider>
+      </ApolloProvider>
+    </>
   );
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="description" content="A Real-time Chat App created with React" />
-        <title>iMessage</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
